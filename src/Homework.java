d6b09d6f4339ee25fc4eabbe0f6fc2984acfda3c
Krakodil <krakodilgena79@bk.ru>
2022-04-19 20:21:00 +0300

    Вроде почти получилось

--- src/Homework.java
+++ src/Homework.java
@@ -1,15 +1,23 @@
-import java.awt.print.Book;
+import java.util.Objects;
 
 public class Homework {
     public static void main(String[] args) {
         Author author = new Author("Дима", "Маслеников");
         Book book = new Book("Book 1 ", author, 2026);
-        System.out.println(book.getTitle() + " " + book.getAuthor().getFirstName() + " "
-                + book.getAuthor().getLastName() + " " + book.getYear());
+        //System.out.println(book.getTitle() + " " + book.getAuthor().getFirstName() + " "
+        //        + book.getAuthor().getLastName() + " " + book.getYear());
 
-        book.setYear(2015)
+        book.setYear(2015);
+         book.toString();
+         book.equals();
+         book.hashCode();
 
-        System.out.println(book.getTitle() + " " + book.getAuthor().getFirstName() + " "
-                + book.getAuthor().getLastName() + " " + book.getYear());
+         author.toString();
+         author.equals();
+         author.hashCode();
+
+        //System.out.println(book.getTitle() + " " + book.getAuthor().getFirstName() + " "
+        //        + book.getAuthor().getLastName() + " " + book.getYear());
     }
+
 }
